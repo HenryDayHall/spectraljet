bdb4fe300381ce128195108a2afe48750d62236b
Henry Day-Hall <henrydayhall@protonmail.com>
2022-01-31 21:59:58 +0100

    fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='spectraljet',
     version='0.1.0',
-    packages=find_packages(include=['spectraljet', 'spectraljet.*'])
+    packages=find_packages(include=['spectraljet', 'spectraljet.*']),
     #url='https://github.com/HenryDayHall/jetTools',
     #long_description=open('README.md').read(),
     install_requires=[
