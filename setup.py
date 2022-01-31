c20ef34c9ccee020f4dccad53c9f8f061ea35cfa
Henry Day-Hall <henrydayhall@protonmail.com>
2022-01-31 22:10:00 +0100

    added requirements.txt to help git actions

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
 
+requirements = []
+with open("requirements.txt", 'r') as req_file:
+    for line in req_file.readlines():
+        line = line.strip()
+        if line[0] == '#':
+            continue
+        if line[-1] == ',':
+            line = line[:-1]
+        requirements.append(line)
+
 setup(
     name='spectraljet',
     version='0.1.0',
     packages=find_packages(include=['spectraljet', 'spectraljet.*']),
     #url='https://github.com/HenryDayHall/jetTools',
     #long_description=open('README.md').read(),
-    install_requires=[
-        "awkward == 0.13.0",
-        "numpy >= 1.16.1",
-        "matplotlib >= 3.1.2",
-        "scipy >= 1.3.3",
-        "pygit2 >= 1.2.1"
-    ]
+    install_requires=requirements
 )
 
