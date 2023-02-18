dc9f73fb427aed3e8d49d69298d5fbbcf4ae35a8
Henry Day-Hall <henrydayhall@protonmail.com>
2023-02-18 12:24:31 +0100

    Made the imports simpler

--- spectraljet/__init__.py
+++ spectraljet/__init__.py
@@ -1,10 +1,5 @@
 # seems to be needed when package is treated as submodule
 # no idea why
-from . import PDGNames
-from . import TypeTools
-from . import Constants
-from . import Components
-from . import FormJets
-from . import FastJetPython
+from . import *
 # also, make EventWise avaliable at top level
 from .Components import EventWise
