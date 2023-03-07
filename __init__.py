5e284a5016c29c7f4f637d2aece78243f944e1ec
Henry Day-Hall <henrydayhall@protonmail.com>
2023-03-07 12:58:27 +0100

    issues with test imports

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
-# allows transparent imports from outside submodule
+# so that modules ar eimported directly from the subdirectory below
 from .spectraljet import *
+# not sure how to fix tests right now
+#from . import test as _test
+#from .test import tools as _test_tools
