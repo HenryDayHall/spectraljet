055766f65ff72e3a1e9277a57e6c9fd68c1c29b7
Henry Day-Hall <henrydayhall@protonmail.com>
2023-04-08 03:48:07 +0200

    removing run_test, just use pytest

--- run_test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-loader = unittest.TestLoader()
-tests = loader.discover(start_dir='test', pattern='test_*.py')
-test_runner = unittest.runner.TextTestRunner()
-test_runner.run(tests)
