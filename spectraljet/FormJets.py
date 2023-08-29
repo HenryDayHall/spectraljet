157fb22ef58634a64c45b2e79456e27017ea5e95
Kieran M <66631199+Kieran-Mg1@users.noreply.github.com>
2023-08-29 11:26:14 +0100

    Added SGWT to form jets + fix
    The fix comes from the fact that at some point in the 10k light higgs events, an error is thrown when a event appears empty. Currently just skips that event.

--- spectraljet/FormJets.py
+++ spectraljet/FormJets.py
@@ -1,9 +1,14 @@
+import sys
+import os
+sys.path.append('sgwt')
+
 import numpy as np
 import time
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import bisect
 from . import Components, Constants
+from sgwt import sgwt_functions 
 import scipy.spatial
 import scipy.linalg
 import awkward as ak
@@ -1939,6 +1944,8 @@ class Partitional(Clustering):
                                 for row in start_ints], dtype=int)
         # at worst, each point gets it's own cluster
         max_elements = n_inputs + n_unclustered
+        if max_elements == 0:
+            raise ValueError("Max elements is zero.")
         assert max_elements <= self.memory_cap, \
             f"More particles ({n_inputs}) than possible " +\
             f"with this memory_cap ({self.memory_cap})"
@@ -2157,6 +2164,85 @@ class IterativeCone(Partitional):
             jet_list.append(cone_labels)
             self._update_avalible(self._avaliable_idxs[cone_avaliable_idxs])
         return jet_list
+
+
+class SGWT(Partitional):
+    default_params = {'Sigma': .11,
+                      'Cutoff': 0,
+                      'Normalised': True,
+                      'NRounds': 15}
+    permited_values = {'Sigma': Constants.numeric_classes['pdn'],
+                       'Cutoff': Constants.numeric_classes['rn'],
+                       'Normalised': [True, False],
+                       'NRounds': Constants.numeric_classes['nn']}
+
+    def _setup_clustering_functions(self):
+        """
+        Assigns internal functions needed in the clustering.
+        Assumes the hyperparams have been set.
+        """
+        pass
+
+    def setup_internal(self):
+        """ Runs before allocate """
+        self.laplacien, self.l_max_val = sgwt_functions.make_L(
+                self.Leaf_Rapidity, self.Leaf_Phi,
+                normalised=self.Normalised, s=self.Sigma)
+
+
+    def allocate(self):
+        """Sort the labels into exclusive jets"""
+
+        available_mask = np.full_like(self.Leaf_Label, True, dtype=bool)
+        jet_list = []
+
+        # Cannot have more jets than input particles.
+        max_jets = min(self.NRounds, available_mask.shape[0])
+
+        l_idx = sgwt_functions.make_L_idx(self.Leaf_Rapidity, self.Leaf_Phi, self.Leaf_PT)
+
+        # Precompute L_idx sum and its sorted indices
+        seed_ordering = l_idx.sum(axis=0).argsort()
+
+        unclustered_idx_pointer = 0  # Pointer to current unclustered index in seed_ordering
+
+        round_counter = 0
+
+        while unclustered_idx_pointer < len(seed_ordering) and round_counter < max_jets:
+
+            if not np.any(available_mask):
+                break  # will also catch the case of empty input
+            
+            next_unclustered_idx = seed_ordering[unclustered_idx_pointer]
+            wavelet_mask = np.zeros_like(self.Leaf_Label, dtype=int)
+            wavelet_mask[next_unclustered_idx] = 1
+
+            # If the current seed is not available (already clustered), skip to the next one
+            if not available_mask[next_unclustered_idx]:
+                unclustered_idx_pointer += 1
+                continue
+
+            _, wp_all = sgwt_functions.wavelet_approx(self.laplacien, self.l_max_val, wavelet_mask)
+            wp_all = np.array(wp_all[0].reshape(-1, 1)).flatten()
+            x = sgwt_functions.min_max_scale(wp_all)
+
+            mask = available_mask * (x < self.Cutoff)
+
+            if np.any(mask):  # Sometimes nothing makes the cutoff.
+                jet_labels = self.Leaf_Label[mask]
+                jet_list.append(jet_labels)
+                available_mask[mask] = False
+
+            unclustered_idx_pointer += 1  # Move to the next unclustered particle
+            round_counter += 1
+
+        # Store anything else as a 1-particle jet
+        jet_list += [self.Leaf_Label[i:i+1] for i in np.where(available_mask)[0]]
+
+        return jet_list
+
+
+
 
 
 def read_jet_contents(eventWise, jet_name):
@@ -2549,6 +2635,13 @@ def cluster_multiapply(eventWise, cluster_algorithm, dict_jet_params={},
             if not silent:
                 print(f"LinAlgError in event {event_n}")
             continue
+        
+        except ValueError as e:  # new exception handling
+            if str(e) == "Max elements is zero.":  # if the error message matches
+                print('Max elements is zero, event might be empty? Skipping...')
+                continue  # break out of the loop
+
+
         duration.append(end_time - start_time)
         jet_list.append(jets)
         if not checked:
