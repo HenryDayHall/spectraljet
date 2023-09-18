28729c23ec3e30b820fbf2c428585f5dd5a21166
Kieran M <66631199+Kieran-Mg1@users.noreply.github.com>
2023-09-18 15:35:52 +0100

    ud

--- spectraljet/FormJets.py
+++ spectraljet/FormJets.py
@@ -2538,7 +2538,6 @@ multiapply_input = cluster_classes
 def get_jet_input_params():
     if hasattr(get_jet_input_params, "_results"):
         return get_jet_input_params._results
-    stack = Clustering.__subclasses__()
     
     stack = [SGWT]  #TODO don't hardcode this
     params = set()
