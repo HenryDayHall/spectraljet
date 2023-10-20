dc2a1827904d65c65631d62024bfd46ab603177e
Henry Day-Hall <henrydayhall@protonmail.com>
2023-10-20 09:48:07 +0200

    beginings of a new aproach to CALE

--- spectraljet/CALEFormJets.py
+++ spectraljet/CALEFormJets.py
@@ -74,6 +74,127 @@ class CALE(FormJets.Partitional):
         return jet_list
 
 
+class CALEv2(FormJets.Partitional):
+    default_params = {'Sigma': .1,
+                      'Cutoff': 0,
+                      'WeightExponent': 0.,
+                      'SeedGenerator': 'PtCenter',
+                      'NRounds': 15}
+    permited_values = {'Sigma': Constants.numeric_classes['pdn'],
+                       'Cutoff': Constants.numeric_classes['rn'],
+                       'WeightExponent': Constants.numeric_classes['pn'],
+                       'SeedGenerator': ['PtCenter', 'Random'],
+                       'NRounds': Constants.numeric_classes['nn']}
+
+    def _setup_clustering_functions(self):
+        """
+        Assigns internal functions needed in the clustering.
+        Assumes the hyperparams have been set.
+        """
+        if self.SeedGenerator == 'PtCenter':
+            self._seed_generator = CALEFunctions.pt_centers
+        elif self.SeedGenerator == 'Random':
+            self._seed_generator = CALEFunctions.random_centers
+        else:
+            raise ValueError('SeedGenerator must be PtCenter or Random')
+
+    def setup_internal(self):
+        """ Runs before allocate """
+        self._seed_labels = []
+
+    def _insert_new_seeds(self):
+        """Inserts new seeds into the clustering"""
+        energies, pxs, pys, pzs = self._seed_generator(
+                self.Avaliable_Energy,
+                self.Avaliable_Px, self.Avaliable_Py, self.Avaliable_Pz)
+        for energy, px, py, pz in zip(energies, pxs, pys, pzs):
+            new_label = np.max(self.Label) + 1
+            self._seed_labels.append(new_label)
+            new_idx = self._next_free_row()
+            # set all relationships to -1
+            # and having Parent=-1
+            self.Child1[new_idx] = -1
+            self.Child2[new_idx] = -1
+            self.Parent[jet_idxs] = -1
+            self.Rank[new_idx] = -1
+            # PT px py pz eta phi energy join_distance
+            self.Energy[new_idx] = energy
+            self.Px[new_idx] = px
+            self.Py[new_idx] = py
+            self.Pz[new_idx] = pz
+            self.Size[new_idx] = 0.
+            # it's easier conceptually to calculate pt, phi and rapidity
+            # afresh than derive them
+            # for some reason this must be unpacked then assigned
+            phi, pt = Components.pxpy_to_phipt(self.Px[new_idx], self.Py[new_idx])
+            self.Phi[new_idx] = phi
+            self.PT[new_idx] = pt
+            self.Rapidity[new_idx] = \
+                Components.ptpze_to_rapidity(self.PT[new_idx], self.Pz[new_idx],
+                                             self.Energy[new_idx])
+
+    def _remove_seeds(self):
+        sorter = np.argsort(self.Label)
+        seed_idxs = sorter[np.searchsorted(self.Label, self._seed_labels,
+                                           sorter=sorter)]
+        # delete all the rows of the float and int arrays containing seeds
+        self._ints = np.delete(self._ints, seed_idxs, axis=0)
+        self._floats = np.delete(self._floats, seed_idxs, axis=0)
+        self._seed_labels.clear()
+
+    def allocate(self):
+        """Sort the labels into exclusive jets"""
+
+        available_mask = np.full_like(self.Leaf_Label, True, dtype=bool)
+        jet_list = []
+
+        self.laplacien = CALEFunctions.pt_laplacian(
+                self.Avaliable_Pt,
+                self.Avaliable_Rapidity, self.Avaliable_Phi,
+                normalised=self.WeightExponent, sigma=self.Sigma)
+
+        # Cannot have more jets than input particles.
+        max_jets = min(self.NRounds, len(self.Leaf_Rapidity))
+
+        l_idx = CALEFunctions.make_L_idx(self.Leaf_Rapidity, self.Leaf_Phi, self.Leaf_PT)
+
+        # Precompute L_idx sum and its sorted indices
+        seed_ordering = l_idx.sum(axis=0).argsort()
+        unclustered_idx_pointer = 0 
+
+        round_counter = 0
+
+        while unclustered_idx_pointer < len(seed_ordering) and round_counter < max_jets:
+            
+            next_unclustered_idx = seed_ordering[unclustered_idx_pointer]
+
+            # If the current seed is not available (already clustered), skip to the next one
+            if not available_mask[next_unclustered_idx]:
+                unclustered_idx_pointer += 1
+                continue
+            wavelet_mask = np.zeros_like(self.Leaf_Label, dtype=int)
+            wavelet_mask[next_unclustered_idx] = 1
+
+            _, wp_all = CALEFunctions.wavelet_approx(self.laplacien, 2, wavelet_mask)
+            wavelet_values = CALEFunctions.min_max_scale(np.array(wp_all[0])).flatten()
+            below_cutoff_indices = set(np.where(wavelet_values < self.Cutoff)[0])
+            available_particles = set(np.where(available_mask)[0])
+            labels = list(below_cutoff_indices & available_particles)
+
+            if labels:  # If we have some labels that match the criteria
+                jet_labels = self.Leaf_Label[labels]
+                jet_list.append(jet_labels)
+                available_mask[labels] = False
+
+            unclustered_idx_pointer += 1
+            round_counter += 1
+
+        # Store anything else as a 1-particle jet
+        jet_list += [self.Leaf_Label[i:i+1] for i in np.where(available_mask)[0]]
+
+        return jet_list
+
+
 class CALECpp(FormJets.Partitional):
     default_params = {'Sigma': .1,
                       'Cutoff': 0,
